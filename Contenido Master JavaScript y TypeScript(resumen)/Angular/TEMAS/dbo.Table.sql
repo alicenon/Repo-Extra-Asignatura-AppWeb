730274dd069ef74c542383a2902964ea7a037daa
Alejandro <77890322+alicenon@users.noreply.github.com>
2024-12-20 12:58:41 +0100

    Add files via upload

---
+++ Contenido Master JavaScript y TypeScript(resumen)/Angular/TEMAS/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Clientes]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY(1,1), 
+    [Nombre] NVARCHAR(50) NULL, 
+    [Ciudad] NVARCHAR(50) NULL, 
+    [Facturacion] MONEY NULL
+)
